7f5f6b481b112e004665212409bf689a3fdb8064
Mertcan Dursun <221903570@ogr.uludag.edu.tr>
2022-04-18 15:54:42 +0300

    last commit

---
+++ Enums/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kategoriler.Elektronik);
+            Console.WriteLine((int)Kategoriler.Elektronik);
+
+            Console.ReadKey();
+        }
+
+        enum Kategoriler
+        {
+            Elektronik = 3,
+            Hırdavat = 2,
+            Gıda = 1,
+            Güzellik = 4,
+            Giyim = 5
+        }
+    }
+}
